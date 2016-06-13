ad603a1e5b9989c20586ccb62116f47d77c206f9
Colin Osborn <colin.osborn@gmail.com>
2016-06-13 10:17:59 -0600

    javaScript

---
+++ main.js
@@ -0,0 +1,41 @@
+var addTwo = function (addend) {
+  return addend + 2;
+};
+
+
+function mathFunction (num) {
+  console.log(num * 2);
+};
+
+
+function sovle(num1, op1, op2) {
+  return op2(op1(num))
+};
+
+function solve(num, op1, op2) {
+  var num1 = op1
+}
+
+cars = ["Ferrari", "McLaren", "Renault", "Mercedes", "Lotus"]
+
+for(var i = 0; i < cars.length; i++) {
+  console.log(cars[i] * 2);
+}
+function addTwo(num) {
+  return num + 2;
+}
+function subtractOne(num) {
+  return num - 1;
+}
+function multiplyByThree(inputNum) {
+  return inputNum * 3;
+}
+function doMath(value, mathFunction) {
+  return mathFunction(value);
+}
+functionArray = [addTwo(2), subtractOne(5), multiplyByThree(3)]
+[4, 4, 9]
+
+for(var i = 0; i < functionArray.length; i++) {
+  console.log(functionArray[i]);
+}
